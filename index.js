a6cc3b74decd4dad7b0a7d9153f9a4009b1a300d
Darren M <aeneascorrupt@gmail.com>
2017-03-06 12:50:50 -0800

    feat(470): improved response structure BREAKING CHANGE: response structure is different
    The validator does well with invalidating a poorly structure
    configuration. However, it was unclear how the caller could respond
    to an error returned from the validator. Was the field supposed to be
    a string? Was the field required? Was the input even valid in the first
    place?
    
    This change addresses it by distinguishing what is an error case and
    what is a valid parse attempt. In addition, it enumerates the errors
    encountered while validating the template configuration.

--- index.js
+++ index.js
@@ -40,11 +40,25 @@ function validateTemplate(templateObj) {
  * Parses the configuration from a screwdriver-template.yaml
  * @method parseTemplate
  * @param  {String} yamlString Contents of screwdriver-template.yaml
- * @return {Promise}           Promise that resolves to the template as a config object
+ * @return {Promise}           Promise that rejects if the configuration cannot be parsed
+ *                             The promise will eventually resolve into:
+ *         {Object}   result
+ *         {Object}   result.template  The parsed template that was validated
+ *         {Object[]} result.errors    An array of objects related to validating
+ *                                     the given template
  */
 function parseTemplate(yamlString) {
     return loadTemplate(yamlString)
-    .then(validateTemplate);
+    .then(configToValidate =>
+        validateTemplate(configToValidate)
+        .then(templateConfiguration => ({
+            errors: [],
+            template: templateConfiguration
+        }), err => ({
+            errors: err.details,
+            template: configToValidate
+        }))
+    );
 }
 
 module.exports = parseTemplate;
