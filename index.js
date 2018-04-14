0260308aa422752a93bcd83f3f48ca051a002d17
Philip Scott <pscott@zeptohost.com>
2018-04-13 17:25:43 -0700

    fix: Make code cross-compatible with node:6

--- index.js
+++ index.js
@@ -38,29 +38,27 @@ function validateTemplate(templateObj) {
 
 /**
  * Parses the configuration from a screwdriver-template.yaml
- * @async  parseTemplate
- * @param  {String} yamlString Contents of screwdriver-template.yaml
- * @return {Promise}           Promise that rejects if the configuration cannot be parsed
- *                             The promise will eventually resolve into:
+ * @method parseTemplate
+ * @param  {String}  yamlString Contents of screwdriver-template.yaml
+ * @return {Promise}            Promise that rejects if the configuration cannot be parsed
+ *                              The promise will eventually resolve into:
  *         {Object}   result
  *         {Object}   result.template  The parsed template that was validated
  *         {Object[]} result.errors    An array of objects related to validating
  *                                     the given template
  */
-async function parseTemplate(yamlString) {
-    const template = await loadTemplate(yamlString);
-
-    try {
-        return {
-            errors: [],
-            template: await validateTemplate(template)
-        };
-    } catch (err) {
-        return {
-            errors: err.details,
-            template
-        };
-    }
+function parseTemplate(yamlString) {
+    return loadTemplate(yamlString)
+        .then(configToValidate =>
+            validateTemplate(configToValidate)
+                .then(templateConfiguration => ({
+                    errors: [],
+                    template: templateConfiguration
+                }), err => ({
+                    errors: err.details,
+                    template: configToValidate
+                }))
+        );
 }
 
 module.exports = parseTemplate;
